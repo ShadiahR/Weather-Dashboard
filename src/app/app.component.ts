455d2182439d7007ca6b1f7f0709c4ddc55845bc
Shadiah Ricardo <49913115+ShadiahR@users.noreply.github.com>
2024-12-06 14:55:05 +0100

    Saving everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [DashboardComponent],
+  //templateUrl: './app.component.html',
+  template: '<app-dashboard></app-dashboard>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'weather-dashboard';
+  title = 'Weather Dashboard';
 }
